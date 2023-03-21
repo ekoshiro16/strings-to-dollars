ae044b5d1db269dc49dbf105515bf288dc78be94
Elle Oshiro <ekoshiro16@gmail.com>
2023-03-20 20:53:24 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+/*
+You are building an ecommerce site, and realize that your backend developer has stored all of the monetary values of your products in your database as numbers. This wouldn't be the biggest problem, except for the fact that you want to convert each number into a specially formatted number using commas, so that the monetary values are easier to read for your users on your site. 
+
+For example:
+1000 should become 1,000
+100000 should become 100,000
+1000000 should become 1,000,000
+35235235 would become 35,235,235
+
+Write an algorithm that accepts an integer argument and converts the integer into a string output that utilizes commas (that is, if the integer needs it. The number 10, for example, does not need any commas). 
+*/
+
+// Write your code below:
